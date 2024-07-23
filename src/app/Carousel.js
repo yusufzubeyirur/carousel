c180e9ed14c8eb65c1376abbdfad45ef58377fc0
yusufzubeyirur <yusufzubeyirur@gmail.com>
2024-07-24 01:40:31 +0300

    caroıselphoto

--- src/app/Carousel.js
+++ src/app/Carousel.js
@@ -1,6 +1,52 @@
-import "./stle.css";
+"use client";
+import { useState } from "react";
+import styles from "./Home.module.css";
+
+const images = [
+  { url: "https://picsum.photos/200/300?random=1", title: "Fotoğraf 1" },
+  { url: "https://picsum.photos/200/300?random=2", title: "Fotoğraf 2" },
+  { url: "https://picsum.photos/200/300?random=3", title: "Fotoğraf 3" },
+  { url: "https://picsum.photos/200/300?random=4", title: "Fotoğraf 4" },
+  { url: "https://picsum.photos/200/300?random=5", title: "Fotoğraf 5" },
+];
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleButtonNext = () => {
+    if (currentIndex < images.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+
+  const handleButtonPrevious = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(images.length - 1);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={handleButtonPrevious}>
+        Önceki⬅️
+      </button>
+      <div className={styles.imageContainer}>
+        <h2 className={styles.title}>CarouselPicture</h2>
+        <img
+          className={styles.image}
+          src={images[currentIndex].url}
+          alt={images[currentIndex].title}
+        />
+      </div>
+      <button className={styles.button} onClick={handleButtonNext}>
+        Sonraki➡️
+      </button>
+    </div>
+  );
 };
+
 export default Carousel;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Carousel from "./Carousel";
-import "./styles.css";
 
 export default function Home() {
   return (
     <>
-      <h1>Carousel</h1>
       <Carousel />
     </>
   );
